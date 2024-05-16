7111a4466b1c51355328f720c2cb6bdfd7557649
blazion09 <muhd09afiq@gmail.com>
2024-05-16 08:57:39 +0800

    chore: add function to retrieve data from weather api
    api key is public

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import "./css/reset.css";
 import "./css/style.css";
+
+async function getWeatherData() {
+  const rawData = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=5ef8c53bb0564f36ab175755241305&q=kuala_lumpur&aqi=no",
+    {
+      mode: "cors",
+    }
+  );
+  const weatherData = await rawData.json();
+  console.log(weatherData.current.condition.text);
+
+  showWeatherData(weatherData);
+}
+
+getWeatherData();
+
+async function showWeatherData(weatherData) {
+  const conditionData = await weatherData.current.condition.text;
+  const iconData = await weatherData.current.condition.icon;
+
+  const p = document.createElement("p");
+  p.textContent = conditionData;
+  document.body.appendChild(p);
+
+  const icon = document.createElement("img");
+  icon.src = iconData;
+  p.appendChild(icon);
+}
