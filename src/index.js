d232b23ae5afaaeba035d83f17430cbc90f87fdf
muhd09afiq <muhd09afiq@gmail.com>
2024-08-07 02:07:58 +0000

    add loading status when fetching

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./css/reset.css";
 import "./css/style.css";
 
 async function getWeatherData(locationValue) {
+  //show loading
+  const loadingText = document.createElement("p");
+  loadingText.textContent = "LOADING DATA";
+  document.body.appendChild(loadingText);
+
   const rawData = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=5ef8c53bb0564f36ab175755241305&q=${locationValue}&aqi=no`,
     {
@@ -10,7 +15,7 @@ async function getWeatherData(locationValue) {
   );
   const weatherData = await rawData.json();
   console.log(weatherData.current.condition.text);
-
+  loadingText.remove();
   showWeatherData(weatherData);
 }
 
